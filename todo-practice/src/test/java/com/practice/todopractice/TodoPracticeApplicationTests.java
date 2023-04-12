8e8c27d2f90a286e8c92933046f7b32b5e46c1bf
King0wl <70518135+yhj0076@users.noreply.github.com>
2023-04-12 22:49:13 +0900

    first commit
    하나로 만드는건지 모르겠어서 아예 파일을 안에 넣었습니당

---
+++ todo-practice/src/test/java/com/practice/todopractice/TodoPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.todopractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodoPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
